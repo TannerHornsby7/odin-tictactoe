71234a1258a7acf30f7d7568529aeab6a340a197
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-09-02 13:36:25 -0500

    Create GameBoard
    Implemented printBoard, checkWinner, and placeValue functions to allow
    easy interaction with player objects later

--- index.js
+++ index.js
@@ -1,5 +1,71 @@
 //Main game logic
 
+//Game Board object
+
+const gameBoard = (() => {
+    let board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+    let placeVal = (val, loc) => {
+        //val 0-8 left to right top to bottom
+        board[parseInt(Math.floor(loc / 3))][loc % 3] = val;
+    }
+    let checkWinner = () => {
+
+        /*
+        8 win conditions:
+            1-6: straights
+            7-8: diagonals
+
+        */
+        for (let i = 0; i < 3; i++) {
+            if(board[0][i] && board[0][i] == board[1][i] && board[1][i] == board[2][i]) { // cols
+                return "colPlayer: " + board[0][i] + " wins!"
+            }
+            if(board[i][0] && board[i][0] == board[i][1] && board[i][1] == board[i][2]) { // rows
+                return "rowPlayer: " + board[i][0] + " wins!"
+            }
+        }
+        if(board[1][1] && board[1][1] == board[0][2] && board[0][2] == board[2][0]) {
+            return "diag Player: " + board[1][1] + " wins!"
+        }
+        if(board[1][1] && board[1][1] == board[0][0] && board[0][0] == board[2][2]) {
+            return "diag Player: " + board[1][1] + " wins!"            
+        }
+    }
+
+
+    let printBoard = () => {
+        for (let i in board) {
+            for (let j in board[i]) {
+                console.log("value at row: " + i + ", and col: " + j + " is " + board[i][j])
+            }
+        }
+    }
+    let clearBoard = () => {
+        for (let i in board) {
+            for (let j in board[i]) {
+                board[i][j] = ""
+            }
+        }
+    }
+
+    return {placeVal, checkWinner, printBoard, clearBoard}
+})();
+
+gameBoard.placeVal("X", 0);
+gameBoard.placeVal("X", 1);
+gameBoard.placeVal("X", 2);
+gameBoard.printBoard();
+console.log(gameBoard.checkWinner());
+gameBoard.placeVal("O", 0);
+gameBoard.printBoard();
+console.log(gameBoard.checkWinner());
+gameBoard.clearBoard();
+gameBoard.printBoard();
+
 
 
 
